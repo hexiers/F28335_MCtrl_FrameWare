8262e8860e5c94671880186bec0bd034a537b78c
Hexer <hex@hust.edu.cn>
2024-06-07 00:53:46 +0800

    删除history文件

--- .history/MotCtl_Users/Source/sendwave_20230905205816.c
+++
@@ -1,7 +0,0 @@
-
-//! @FilePath: \DSP28xx_MotCtl_Frameware\MotCtl_Users\Source\sendwave.c
-//! @Author: Hex
-//! @Date: 2023-09-05 20:58:06
-//! @LastEditTime: 2023-09-05 20:58:16
-//! @Description: 
-
